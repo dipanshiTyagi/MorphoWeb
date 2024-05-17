4dbd2899447fcaec33e2b225d90414ec1ad5ae24
Ahmad Iqbal Bhatti <ahmediqbalbhatti1@gmail.com>
2024-05-18 00:32:21 +0500

    Adding Agency Section

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,29 +1,39 @@
+export const SIGN_IN = "/sign-in";
+export const SIGN_UP = "/sign-up";
+export const AGENCY = "/agency";
+export const SUBACCOUNT = "/subaccount";
+
+export const AGENCY_OWNER = "AGENCY_OWNER";
+export const AGENCY_ADMIN = "AGENCY_ADMIN";
+export const SUBACCOUNT_USER = "SUBACCOUNT_USER";
+export const SUBACCOUNT_GUEST = "SUBACCOUNT_GUEST";
+
 export const pricingCards = [
   {
-    title: 'Starter',
-    description: 'Perfect for trying out plura',
-    price: 'Free',
-    duration: '',
-    highlight: 'Key features',
-    features: ['3 Sub accounts', '2 Team members', 'Unlimited pipelines'],
-    priceId: '',
+    title: "Starter",
+    description: "Perfect for trying out plura",
+    price: "Free",
+    duration: "",
+    highlight: "Key features",
+    features: ["3 Sub accounts", "2 Team members", "Unlimited pipelines"],
+    priceId: "",
   },
   {
-    title: 'Unlimited Saas',
-    description: 'The ultimate agency kit',
-    price: '$199',
-    duration: 'month',
-    highlight: 'Key features',
-    features: ['Rebilling', '24/7 Support team'],
-    priceId: 'price_1OYxkqFj9oKEERu1KfJGWxgN',
+    title: "Unlimited Saas",
+    description: "The ultimate agency kit",
+    price: "$199",
+    duration: "month",
+    highlight: "Key features",
+    features: ["Rebilling", "24/7 Support team"],
+    priceId: "price_1OYxkqFj9oKEERu1KfJGWxgN",
   },
   {
-    title: 'Basic',
-    description: 'For serious agency owners',
-    price: '$49',
-    duration: 'month',
-    highlight: 'Everything in Starter, plus',
-    features: ['Unlimited Sub accounts', 'Unlimited Team members'],
-    priceId: 'price_1OYxkqFj9oKEERu1NbKUxXxN',
+    title: "Basic",
+    description: "For serious agency owners",
+    price: "$49",
+    duration: "month",
+    highlight: "Everything in Starter, plus",
+    features: ["Unlimited Sub accounts", "Unlimited Team members"],
+    priceId: "price_1OYxkqFj9oKEERu1NbKUxXxN",
   },
-]
+];
