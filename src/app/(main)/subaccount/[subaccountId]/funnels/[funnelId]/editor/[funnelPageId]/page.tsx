d746ae794ab270494905a853c55c4abed80b3deb
Ahmad Iqbal Bhatti <ahmediqbalbhatti1@gmail.com>
2024-06-07 16:27:23 +0500

    Settings tab Created

--- src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/page.tsx
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/page.tsx
@@ -1,7 +1,10 @@
 import { SUBACCOUNT_SLUG } from "@/lib/constants";
 import { db } from "@/lib/db";
+import EditorProvider from "@/providers/editor/editor-provider";
 import { redirect } from "next/navigation";
 import React from "react";
+import FunnelEditorNavigation from "./_components/funnel-editor-navigation";
+import FunnelEditorSidebar from "./_components/funnel-editor-sidebar";
 
 type Props = {
   params: {
@@ -24,7 +27,18 @@ const FunnelPageId = async ({ params }: Props) => {
 
   return (
     <div className="fixed top-0 bottom-0 left-0 right-0 z-20 bg-background overflow-hidden">
-        
+      <EditorProvider
+        subaccountId={params.subaccountId}
+        funnelId={params.funnelId}
+        pageDetails={funnelPageDetails}
+      >
+        <FunnelEditorNavigation
+          funnelId={params.funnelId}
+          subaccountId={params.subaccountId}
+          funnelPageDetails={funnelPageDetails}
+        />
+        <FunnelEditorSidebar subaccountId={params.subaccountId} />
+      </EditorProvider>
     </div>
   );
 };
